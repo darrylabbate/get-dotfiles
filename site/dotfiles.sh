0a2c7a538f51aa4d3421ece4d3175e5cdd7e367f
Darryl Abbate <rootbeersoup@gmail.com>
2018-04-10 02:41:03 -0500

    Update install script

--- site/dotfiles.sh
+++ site/dotfiles.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-# This is a placeholder
+# Install git function
+function installgit() {
+	if [ "$(uname -s)" == "Darwin" ]; then
+		xcode-select --install
+	elif [ "$(uname -s)" == "Linux" ]; then
+		sudo apt-get install git -y
+	fi
+}
+
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo ""
+echo "dotfiles"
+echo ""
+echo "WARNING: This will install new files and may overwrite some existing files in your home directory."
+echo ""
+read -p "  Continue? <Y/N> " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Installing dotfiles..."
+fi
+
+installgit
+git clone --recursive https://github.com/rootbeersoup/dotfiles.git ~
+cd dotfiles
+make
