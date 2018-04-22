d6d3df3d854fe696181c8f3f88ed23449e4f609a
Darryl Abbate <rootbeersoup@gmail.com>
2018-04-22 14:31:00 -0500

    Beautify and document script

--- site/dotfiles.sh
+++ site/dotfiles.sh
@@ -1,19 +1,45 @@
 #!/usr/bin/env bash
+#
+#     /\\\                                  /\\\\\        /\\\\\\                              
+#     \/\\\                                /\\\///   /\\\ \////\\\                              
+#      \/\\\                  /\\\         /\\\      \///     \/\\\                              
+#       \/\\\     /\\\\\    /\\\\\\\\\\\ /\\\\\\\\\ /\\\\\\    \/\\\       /\\\\\\\\   /\\\\\\\\\\
+#   /\\\\\\\\\   /\\\///\\\ \////\\\//// \////\\\// \////\\\    \/\\\     /\\\/////\\\ \/\\\//////
+#   /\\\////\\\  /\\\  \//\\\   \/\\\        \/\\\      \/\\\    \/\\\    /\\\\\\\\\\\  \/\\\\\\\\\\
+#   \/\\\  \/\\\ \//\\\  /\\\    \/\\\ /\\    \/\\\      \/\\\    \/\\\   \//\\///////   \////////\\\
+#    \//\\\\\\\/\\ \///\\\\\/     \//\\\\\     \/\\\      \/\\\    \/\\\\\\ \//\\\\\\\\\\  /\\\\\\\\\\
+#      \///////\//    \/////        \/////      \///       \///     \//////   \//////////  \//////////
+#
+#
+#	This is an installation script for Darryl Abbate's dotfiles
+#	https://github.com/rootbeersoup/dotfiles
+#
+#	Install by running this command:
+#	curl get.darryl.sh | sh
 
-# Install git function
+
+# Install git
+
 function installgit() {
-	if [ "$(uname -s)" == "Darwin" ]; then
+	if [ "$(uname -s)" == "Darwin" ]; then  # macOS
 		xcode-select --install
-	elif [ "$(uname -s)" == "Linux" ]; then
+	elif [ "$(uname -s)" == "Linux" ]; then # Ubuntu
 		sudo apt-get install git -y
 	fi
 }
 
+
+# Keep sudo alive for the entire session
+
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+
+# Ask for confirmation before proceeding
+# Script will proceed upon press of 'Y' key (case-insensitive); It will not listen for a carriage return
+
 echo ""
-echo "dotfiles"
+echo "Darryl Abbate's dotfiles"
 echo ""
 echo "WARNING: This will install new files and may overwrite some existing files in your home directory."
 echo ""
@@ -24,7 +50,9 @@ then
 	echo "Installing dotfiles..."
 fi
 
-installgit
-git clone --recursive https://github.com/rootbeersoup/dotfiles.git ~
-cd dotfiles
-make
+# Do the thing
+
+installgit                                                            # Install git
+git clone --recursive https://github.com/rootbeersoup/dotfiles.git ~  # Clone the dotfiles repo to the home directory
+cd dotfiles                                                           # Enter ~/dotfiles
+make                                                                  # Invoke the Makefile
