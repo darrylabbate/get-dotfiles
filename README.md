764b6fa21e1db10d7649829dca3b848f6f0830e3
Darryl Abbate <rootbeersoup@gmail.com>
2018-07-05 22:09:20 -0500

    Re-format for 100% POSIX sh compliance
    Thanks shellcheck
    
    SHA-256: 4ed98f0e24872b32f0d123eb98f1a7d579eb49283e390411bdf544c68e756957

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repository is a mirror for the subdomain of my personal site. Currently it contains a single shell script which can be invoked with `curl` to clone and install my [dotfiles](https://github.com/rootbeersoup/dotfiles). This provides the absolute easiest installation method should I have the misfortune of having to set up a new macOS device.
 
-The SHA-256 checksum of the script is `dd15ffb9e85a226d67c4a175518ae3e62f2c513b27d0ecc5093db7a1e9b463c2`
+The SHA-256 checksum of the script is `4ed98f0e24872b32f0d123eb98f1a7d579eb49283e390411bdf544c68e756957`
 
 You can (and should) verify the SHA-256 checksum before downloading. This can be done via `shasum -a 256`
 
--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -22,7 +22,7 @@
 # Exit if a dotfiles directory already exists
 
 if [ -d "$HOME/dotfiles" ]; then
-  printf "\033[31mERROR:\033[0m dotfiles is already installed on this system. Exiting.\n" >&2
+  printf "\\033[31mERROR:\\033[0m dotfiles is already installed on this system. Exiting.\\n" >&2
   exit 1
 fi
 
@@ -35,15 +35,15 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Display info
 
-printf "\n\033[1mDOTFILES\033[0m\n"
-printf "\n\033[31mWARNING:\033[0m This will install new files and may overwrite\nsome existing files in your home directory.\n"
-printf "\nContinue? (y/n)\n"
-read -s -n 1 REPLY
-if ! [ $REPLY = Y -o $REPLY = y ]; then
-  printf "\nProcess terminated by user.\n" >&2
-  exit 1
+printf "\\n\\033[1mDOTFILES\\033[0m\\n"
+printf "\\n\\033[31mWARNING:\\033[0m This will install new files and may overwrite\\nsome existing files in your home directory.\\n"
+printf "\\nContinue? (y/n)\\n"
+read -r REPLY
+if [ "$REPLY" = Y ] || [ "$REPLY" = y ]; then
+  printf "\\nInstalling dotfiles...\\n"
 else
-  printf "\nInstalling dotfiles...\n"
+  printf "\\nProcess terminated by user.\\n" >&2
+  exit 1
 fi
 
 
@@ -62,6 +62,6 @@ fi
 
 # Clone the dotfiles repository from GitHub and invoke the Makefile
 
-git clone --recursive https://github.com/rootbeersoup/dotfiles.git $HOME/dotfiles
-cd $HOME/dotfiles
+git clone --recursive https://github.com/rootbeersoup/dotfiles.git "$HOME/dotfiles"
+cd "$HOME/dotfiles" || exit 1
 make
