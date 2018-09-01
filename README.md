14f2f40289de691fdb28d46571b2660758db966a
Darryl Abbate <rootbeersoup@gmail.com>
2018-09-01 14:05:14 -0500

    Use single script
    This changes the command to a single URL, with the script providing the
    option of invoking a full installation or simply making symlinks.
    
    SHA-256: 63e5f7771799dac27d83884accd4ebf8d3d602ef3343e0880258537020be4068

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # `get.darryl.sh`
 
-A portable toolkit; a collection of scripts designed to be invoked remotely for easy access on any Unix-like system.
+Shell script for remotely installing [dotfiles](https://github.com/rootbeersoup/dotfiles), to be invoked with `curl` or `wget`.
 
 ## Usage
 
-Command | Output
---------|-------
-`curl get.darryl.sh` | Print basic usage information
-`curl get.darryl.sh/dotfiles \| sh` | Install [dotfiles](https://github.com/rootbeersoup/dotfiles)
-`curl get.darryl.sh/lite \| sh` | Source a minimal Bash configuration
+```bash
+$ curl get.darryl.sh | sh
+```
+
+```bash
+$ wget -qO- get.darryl.sh | sh
+```
+
+Read the prompt and choose between full installation or symlink-only installation.
 
 ## Checksums
 
-Script | SHA-256 Checksum
--------|-----------------
-[dotfiles.sh](https://github.com/rootbeersoup/get.darryl.sh/blob/master/src/dotfiles.sh) | `5e235d8c44c5f3eab3f04d999281349cde47b34d26328293b66b4325f815de62`
-[lite.bash](https://github.com/rootbeersoup/get.darryl.sh/blob/master/src/lite.bash) | `a109bffa376f23fe4842d7946617c18d9d7412165c5ded18d2bd0c3b4db314d0`
+Algorithm | Checksum
+----------|---------
+MD5       | `ea534914c41f6987b38306148f74c2a4`
+SHA-1     | `d5708ca1db124027f911b78661941b359cc05ebb`
+SHA-256   | `63e5f7771799dac27d83884accd4ebf8d3d602ef3343e0880258537020be4068`
+
 
 While `curl | sh` is considered an "unsafe" practice by many, we can mitigate risk by validating checksums of the scripts before piping them directly to `sh`. To check the SHA-256 checksum of the dotfiles installer:
 
--- src/dotfiles.sh
+++ src/dotfiles.sh
@@ -6,43 +6,99 @@
 # https://github.com/rootbeersoup/dotfiles
 #
 # Install by running either command:
-# curl -fsSL get.darryl.sh/dotfiles | sh
-# wget -qO- get.darryl.sh/dotfiles | sh
+# curl -fsSL get.darryl.sh | sh
+# wget -qO- get.darryl.sh | sh
 
 # Exit if a dotfiles directory already exists
-if [ -d "$HOME/dotfiles" ]; then
-  printf "\\033[31mERROR:\\033[0m dotfiles is already installed on this system.\\n" >&2
-  exit 1
-fi
-
-# Display warning and prompt user to confirm before proceeding
-printf "\\n\\033[1mDOTFILES\\033[0m\\n"
-printf "\\n\\033[31mWARNING:\\033[0m This will install new files and may overwrite\\nsome existing files in your home directory.\\n"
-printf "\\nContinue? (y/n)\\n"
-read -r REPLY
-if [ "$REPLY" = Y ] || [ "$REPLY" = y ]; then
-  printf "\\nInstalling dotfiles...\\n"
-else
-  printf "\\nProcess terminated by user.\\n" >&2
-  exit 1
-fi
-
-# Keep sudo alive for the entire session
-sudo -v
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Install git if not already installed
-# Install Homebrew on macOS since it installs Xcode Command-Line Tools automatically
-if ! [ -x "$(command -v git)" ]; then
-  if [ "$(uname -s)" = "Darwin" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install git
-  elif [ "$(uname -s)" = "Linux" ]; then
-    sudo apt-get install git -y
+dotfiles_check ()
+{
+  if [ -d "$HOME/dotfiles" ]; then
+    printf "\\033[31mERROR:\\033[0m ~/dotfiles already exists on this system.\\n" >&2
+    exit 1
   fi
-fi
+}
 
-# Clone the dotfiles repository from GitHub and invoke the Makefile
-git clone --recursive https://github.com/rootbeersoup/dotfiles.git "$HOME/dotfiles"
-cd "$HOME/dotfiles" || exit 1
-make
+# Full installation function
+make_install ()
+{
+  # Keep sudo alive for the entire session
+  sudo -v
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+  # Install git if not already installed
+  # Install Homebrew on macOS since it installs Xcode Command-Line Tools automatically
+  if ! [ -x "$(command -v git)" ]; then
+    if [ "$(uname -s)" = "Darwin" ]; then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      brew install git
+    elif [ "$(uname -s)" = "Linux" ]; then
+      sudo apt-get install git -y
+    fi
+  fi
+  git clone --recursive https://github.com/rootbeersoup/dotfiles.git "$HOME/dotfiles"
+  cd "$HOME/dotfiles" || exit 1
+  make
+}
+
+# Temp symlink function
+# Download tar.gz if git isn't installed
+make_links ()
+{
+  if ! [ -x "$(command -v git)" ]; then
+    if [ -x "$(command -v curl)" ]; then
+      curl -L https://github.com/rootbeersoup/dotfiles/archive/master.tar.gz -o "$HOME/dotfiles-master.tar.gz"
+      tar xzf "$HOME/dotfiles-master.tar.gz"
+      mv "$HOME/dotfiles-master" "$HOME/dotfiles"
+      cd "$HOME/dotfiles" || exit 1
+      make links
+    elif [ -x "$(command -v wget)" ]; then
+      wget -O "$HOME/dotfiles-master.tar.gz" https://github.com/rootbeersoup/dotfiles/archive/master.tar.gz
+      tar xzf "$HOME/dotfiles-master.tar.gz"
+      mv "$HOME/dotfiles-master" "$HOME/dotfiles"
+      cd "$HOME/dotfiles" || exit 1
+      make links
+    fi
+  else
+    git clone --recursive https://github.com/rootbeersoup/dotfiles.git "$HOME/dotfiles"
+    cd "$HOME/dotfiles" || exit 1
+    make links
+  fi
+}
+
+main ()
+{
+  # Display warning and prompt user to confirm before proceeding
+  printf "\\n\
+\\033[1mDOTFILES\\033[0m\\n\
+\\n\
+Custom macOS settings and terminal configurations.\\n\
+https://github.com/rootbeersoup/dotfiles\\n\
+\\n\
+\\033[1mOPTIONS:\\033[0m\\n\
+\\n\
+  Full        Full installation, which will install new programs.\\n\
+              Suitable for personal computer setup.\\n\
+\\n\
+  Symlinks    Only make symlinks to Bash and Vim configurations.\\n\
+              Suitable for temporary or shared computer configuration.\\n\
+\\n\
+\\033[31mWARNING:\\033[0m This will install new files and may overwrite some existing\\n\
+         files in your home directory.\\n\
+\\n\
+Continue? ([F]ull, [S]ymlinks, [C]ancel)\\n\
+> "
+  read -r REPLY
+  if [ "$REPLY" = F ] || [ "$REPLY" = f ]; then
+    printf "Installing dotfiles...\\n"
+    make_install
+  elif [ "$REPLY" = S ] || [ "$REPLY" = s ]; then
+    printf "Downloading dotfiles and creating symlinks...\\n"
+    make_links
+  else
+    printf "Process terminated by user.\\n" >&2
+    exit 1
+  fi
+}
+
+dotfiles_check
+main
