d869a247f04141b4fe5565b7eee58021abf42da7
Darryl Abbate <rootbeersoup@gmail.com>
2022-07-27 17:59:55 -0700

    Update script

--- src/dotfiles.sh
+++ src/dotfiles.sh
@@ -1,53 +1,53 @@
 #!/bin/sh -e
 #
-# dotfiles repository download script
+# dotfiles repo download script
 #
 # Author: Darryl Abbate
-# https://github.com/rootbeersoup/dotfiles
+# https://github.com/darrylabbate/dotfiles
 #
 # Run with either command:
 # curl -fsSL get.darryl.cx | sh
 # wget -qO- get.darryl.cx | sh
 
+REPO=https://github.com/darrylabbate/dotfiles
+
 # Exit if a dotfiles directory already exists
-dotfiles_check ()
-{
-  if [ -d "$HOME/dotfiles" ]; then
-    printf "\\033[31mERROR:\\033[0m ~/dotfiles already exists on this system.\\n" >&2
-    exit 1
-  fi
+dotfiles_check() {
+    if [ -d "$HOME/dotfiles" ]; then
+        printf "\\033[31mERROR:\\033[0m ~/dotfiles already exists on this system.\\n" >&2
+        exit 1
+    fi
 }
 
-main()
-{
-  # If git isn't installed
-  if ! [ -x "$(command -v git)" ]; then
-    
-    # Download repo tar.gz via curl
-    if [ -x "$(command -v curl)" ]; then
-      curl -L https://github.com/rootbeersoup/dotfiles/archive/master.tar.gz -o "$HOME/dotfiles-master.tar.gz"
-      tar xzf "$HOME/dotfiles-master.tar.gz"
-      mv "$HOME/dotfiles-master" "$HOME/dotfiles"
-    
-    # Else, download repo tar.gz via wget
-    elif [ -x "$(command -v wget)" ]; then
-      wget -O "$HOME/dotfiles-master.tar.gz" https://github.com/rootbeersoup/dotfiles/archive/master.tar.gz
-      tar xzf "$HOME/dotfiles-master.tar.gz"
-      mv "$HOME/dotfiles-master" "$HOME/dotfiles"
-    fi
+main() {
+    # If git isn't installed
+    if ! [ -x "$(command -v git)" ]; then
+        
+        # Download repo tar.gz via curl
+        if [ -x "$(command -v curl)" ]; then
+            curl -L "$REPO/archive/master.tar.gz" -o "$HOME/dotfiles-master.tar.gz"
+            tar xzf "$HOME/dotfiles-master.tar.gz"
+            mv "$HOME/dotfiles-master" "$HOME/dotfiles"
+        
+        # Else, download repo tar.gz via wget
+        elif [ -x "$(command -v wget)" ]; then
+            wget -O "$HOME/dotfiles-master.tar.gz" "$REPO/archive/master.tar.gz"
+            tar xzf "$HOME/dotfiles-master.tar.gz"
+            mv "$HOME/dotfiles-master" "$HOME/dotfiles"
+        fi
 
-  # Else, clone the dotfiles repo via git
-  else
-    git clone --recursive https://github.com/rootbeersoup/dotfiles.git "$HOME/dotfiles"
-  fi
-  
-  # Verify successful download and print instructions for the user
-  if [ -d "$HOME/dotfiles" ]; then
-    printf "\\033[32mSUCCESS:\\033[0m Repository downloaded to ~/dotfiles\\nNavigate to ~/dotfiles and run \`make help\` for usage info\\n"
-  else
-    printf "\\033[31mERROR:\\033[0m dotfiles either not downloaded or not extracted successfully\\n" >&2
-    exit 1
-  fi
+    # Else, clone the dotfiles repo via git
+    else
+        git clone --recursive "$REPO.git" "$HOME/dotfiles"
+    fi
+    
+    # Verify successful download and print instructions for the user
+    if [ -d "$HOME/dotfiles" ]; then
+        printf "\\033[32mSUCCESS:\\033[0m Repository downloaded to ~/dotfiles\\nNavigate to ~/dotfiles and run \`make help\` for usage info\\n"
+    else
+        printf "\\033[31mERROR:\\033[0m dotfiles either not downloaded or not extracted successfully\\n" >&2
+        exit 1
+    fi
 }
 
 dotfiles_check
